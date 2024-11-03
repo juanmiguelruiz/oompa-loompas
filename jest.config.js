3993cfb08b13ed4919df899b575a78026cd09be6
Juan Miguel Ruiz Pazos <juanmiguelruizpazos@gmail.com>
2024-11-03 20:02:22 +0100

    chore: testing setup

---
+++ jest.config.js
@@ -0,0 +1,32 @@
+export default {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+          },
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/tests/__mocks__/fileMock.ts',
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^components/(.*)$': '<rootDir>/src/components/$1',
+    '^constants/(.*)$': '<rootDir>/src/constants/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
