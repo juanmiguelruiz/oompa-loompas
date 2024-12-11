ad933f6d6c16fc31f95295fe2edc938f8c40b548
Juan Miguel Ruiz Pazos <juanmiguelruizpazos@gmail.com>
2024-12-11 11:33:26 +0100

    feat: enhance OompaLoompas API response structure

--- src/services/oompaLompas.ts
+++ src/services/oompaLompas.ts
@@ -1,10 +1,22 @@
-import { OompaLoompaDetail, OompaLoompasListResponse } from 'types';
+import { OompaLoompa, OompaLoompaDetail, OompaLoompasListResponse } from 'types';
 
 const API_URL = 'https://2q2woep105.execute-api.eu-west-1.amazonaws.com/napptilus/oompa-loompas';
 
 const getOompaLoompasList = async (page = 1): Promise<OompaLoompasListResponse> => {
   const response = await fetch(`${API_URL}?page=${page}`);
-  return response.json();
+  const data = await response.json();
+  return {
+    current: data.current,
+    total: data.total,
+    results: data.results.map((oompa: OompaLoompa) => ({
+      id: oompa.id,
+      first_name: oompa.first_name,
+      last_name: oompa.last_name,
+      image: oompa.image,
+      gender: oompa.gender,
+      profession: oompa.profession,
+    })),
+  };
 };
 
 const getOompaLoompaById = async (id: number): Promise<OompaLoompaDetail> => {
